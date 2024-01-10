14267f2e248b1842ca7475a893e7d4481b65022a
Sergej080295 <milowanow-sergej@mail.ru>
2024-01-10 09:03:21 +0300

    Добавил задиния по 2 лекции

---
+++ lesson2/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача №1 Создать массив и заполнить его целыми числами от 1 до N (N - произвольное натуральное число). Вывести элементы массива на экран.
+
+
+int N = 10;
+int[] arr = new int[N];
+int i = 0;
+
+while (i < N)
+{
+    arr[i] = i + 1;
+    //Console.WriteLine(arr[i]);
+    //Console.Write(arr[' ']);
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
