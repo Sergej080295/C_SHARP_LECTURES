3acf92a02fdc75e16c73f376a535094356435e2c
Sergej080295 <milowanow-sergej@mail.ru>
2024-01-23 21:44:13 +0300

    Добавил решение 3 задач по 4 лекции

---
+++ lesson4/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача №3
+- Считать с консоли строку, состоящую из 
+цифр и латинских букв.
+- Сформировать новую строку, состоящую из 
+букв исходной строки.
+*/
+
+string GetLettersFromStr(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string? str = Console.ReadLine();
+if (str != null)
+{
+    string result = GetLettersFromStr(str);
+    Console.WriteLine(result);
+}
