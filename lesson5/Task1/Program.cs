d21d3e83bd1cc2c28b4e1db0a74db4dea0338f14
Sergej080295 <milowanow-sergej@mail.ru>
2024-02-01 18:52:32 +0300

    Добавил задачи по 5 лекции

---
+++ lesson5/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Нахождение факторила
+
+int Fact(int n)
+{
+    if (n ==1 || n == 0)
+    {
+        Console.WriteLine($"Stop requrson:n={n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+
+Console.Write(Fact(5));
